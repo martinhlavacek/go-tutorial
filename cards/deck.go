aec7be33c2178046132c808850b7d096c9007f7d
Martin Hlavacek <martin@martinhlavacek.cz>
2018-01-26 22:06:49 +0100

    save data to file

--- cards/deck.go
+++ cards/deck.go
@@ -1,14 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+)
 
 // create a new type of deck
 // which is a slice of strings
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string {"one", "two"}
+	cardValues := []string {"a", "b"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues{
+			cards = append(cards, suit+" of "+ value)
+		}
+	}
+	return cards
+}
 
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	//[]string(d)
+	return strings.Join(d,",")
+	//return strings.Join([]string(d),",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
 
--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,26 @@
 package main
 
+import "fmt"
+
 func main(){
-	cards := deck{newCart("Tomas"), newCart("Martin")}
-	cards = append(cards, newCart("John"))
-	cards.print()
+	//cards := deck{newCart("Tomas"), newCart("Martin")}
+	//cards = append(cards, newCart("John"))
+	cards := newDeck()
+	fmt.Println(cards.toString())
+	fmt.Println([]byte(cards.toString()))
+	cards.saveToFile("test.txt")
+	// hand, remainingCards := deal(cards, 2)
+	
+	// hand.print()
+	// remainingCards.print()
+
+	//cards.print()
+
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
 
-func newCart(val string) string {
-	return "Hi " + val
-}
+
+// func newCart(val string) string {
+// 	return "Hi " + val
+// }

872f6f1bee9ba9ae69dabdd92fe63705675d8df0
Martin Hlavacek <martin@martinhlavacek.cz>
2018-01-27 10:40:55 +0100

    extend struct

--- structs/main.go
+++ structs/main.go
@@ -2,18 +2,32 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zip int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
 	// 1. example
-	alex := person{"Alex", "Novak"}
-	john := person{firstName: "John", lastName:"Novak"}
-
+	alex := person{firstName: "Alex",lastName: "Novak", contact: contactInfo{email: "asd@asd.cz",zip: 14111}}
+	john := person{
+		firstName: 	"John", 
+		lastName:	"Novak",
+		contact: contactInfo{
+			email: "ll$asd.com",
+			zip: 15000,
+		},
+	}
+	fmt.Println("Alex")
 	fmt.Println(alex)
-	fmt.Println(john)
+	fmt.Println("John")
+	fmt.Printf("%+v\n", john)
 
 	// 2. example update
 	var martin person
@@ -21,6 +35,7 @@ func main() {
 	martin.firstName = "Martin"
 	martin.lastName = "Hlavacek"
 
+	fmt.Println("Martin")
 	fmt.Println(martin)
 	fmt.Printf("%+v", martin)
 

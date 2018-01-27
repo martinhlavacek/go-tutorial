4299489747524e98bfebbf10c954ae168c6c2e53
Martin Hlavacek <martin@martinhlavacek.cz>
2018-01-27 10:29:48 +0100

    update structs

--- structs/main.go
+++ structs/main.go
@@ -8,10 +8,22 @@ type person struct {
 }
 
 func main() {
+	// 1. example
 	alex := person{"Alex", "Novak"}
 	john := person{firstName: "John", lastName:"Novak"}
 
 	fmt.Println(alex)
 	fmt.Println(john)
+
+	// 2. example update
+	var martin person
+
+	martin.firstName = "Martin"
+	martin.lastName = "Hlavacek"
+
+	fmt.Println(martin)
+	fmt.Printf("%+v", martin)
+
+
 
 }
